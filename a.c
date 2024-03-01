25855883d08cec4ed0a75be3cf35ef8f4b7ad9d2
Junzhe Yu <yujzh1@shanghaitech.edu.cn>
2024-03-01 19:12:56 +0800

    more responsive on circle button release; recover right button state on release

--- a.c
+++ a.c
@@ -1,13 +1,18 @@
 #include <windows.h>
 #include <stdio.h>
 
+const int brakeKey = 'Z';
+const int circleKey = 'X';
+const int centerCompensate = 0;
+
 DWORD WINAPI ClickThread(LPVOID lpParam) {
     int* pos = (int*)lpParam;
     mouse_event(MOUSEEVENTF_RIGHTDOWN, pos[0], pos[1], 0, 0);
-    Sleep(300); // 延迟0.3秒
+    Sleep(20); // 延迟0.3秒
     mouse_event(MOUSEEVENTF_RIGHTUP, pos[0], pos[1], 0, 0);
     return 0;
 }
+
 
 int main() {
     INPUT ip;
@@ -16,35 +21,35 @@ int main() {
     ip.ki.time = 0;
     ip.ki.dwExtraInfo = 0;
 
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    int centerX = screenWidth / 2 + centerCompensate;
+    int centerY = screenHeight / 2 + centerCompensate;
+
     // Listen for a key press
     while (1) {
-        if (GetAsyncKeyState(VK_F7)) {  // replace 'C' with the key you want
+        if (GetAsyncKeyState(brakeKey)) {  // replace 'C' with the key you want
             // printf("The 'C' key is pressed.\n");
 
-            // Get the screen dimensions
-            int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-            int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-
             // Set the cursor position to the center of the screen
-            SetCursorPos(screenWidth / 2 + 3, screenHeight / 2 + 3);
+            SetCursorPos(centerX, centerY);
 
             // // Wait until the key is released
             // while (GetAsyncKeyState(VK_F8)) {}
         }
         
-        if (GetAsyncKeyState(VK_F8)) {
+        if (GetAsyncKeyState(circleKey)) {
             const double PI = 3.14159265358979323846;
             const int radius = 10; // 设置旋转半径
-            const int clicks = 3; // 设置点击次数
+            const int clicks = 1; // 设置点击次数
             const int stepsPerRound = 10; // 设置旋转的步数，现在是两圈
-            const int nRounds = 3;
-            int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-            int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-            int centerX = screenWidth / 2 + 3;
-            int centerY = screenHeight / 2 + 3;
+            const int nRounds = 1;
 
             int stepTot = stepsPerRound * nRounds;
             int stepsPerClick = stepTot / clicks;
+
+            SHORT rightButtonState = GetAsyncKeyState(VK_RBUTTON);
+            HANDLE hThread;
 
             for (int roundIdx = 0; roundIdx < nRounds; roundIdx++) {
                 for (int stepIdx = 0; stepIdx < stepsPerRound; stepIdx++) {
@@ -57,15 +62,23 @@ int main() {
                     if (stepTotCur % stepsPerClick == 0) {
                         // 模拟鼠标右键点击
                         int pos[2] = {x, y};
-                        CreateThread(NULL, 0, ClickThread, pos, 0, NULL);
+                        hThread = CreateThread(NULL, 0, ClickThread, pos, 0, NULL);
                     }
 
-                    Sleep(15); // 延迟以便在1秒内完成旋转
+                    Sleep(20); // 延迟以便在1秒内完成旋转
                 }
             }
 
-            // move the cursor to the center
-            SetCursorPos(centerX, centerY);
+            end:
+                WaitForSingleObject(hThread, INFINITE);
+                // move the cursor to the center
+                SetCursorPos(centerX, centerY);
+                // Restore the state of the right button
+                if (rightButtonState & 0x8000) {
+                    // If the right button was down before, simulate a right button down event
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                    // printf("right button down\n");
+                }
         }
     }
 
